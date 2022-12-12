e3804f04960f716859936e784e6654540aa6d954
Wediah <emmanuelwediah@gmail.com>
2022-12-12 13:09:06 +0000

    packages added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main >
-        
+        <Navbar/>
       </main>
     </div>
   )
