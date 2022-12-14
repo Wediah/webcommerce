43cbaad2d482206fc13743c878fc212a2467e5c7
Wediah <emmanuelwediah@gmail.com>
2022-12-14 15:04:40 +0000

    styled components removed

--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import { BsSearch } from 'react-icons/bs'
-import { useState } from "react";
 
 
-function Searchbar() {
+export default function Searchbar(props) {
   return (
-    <div className=' text-center p-10 '>
-      <form action="" className='bg-transparent shadow-2xl'>
-        
-        <input type="text" placeholder='Your next destination ' className="search w-3/4 h-10 rounded-full " />
-        <button type='submit' className=' p-3 h-10 text-white'><BsSearch/></button>
-      </form>
-    </div>
+    <div>Searchbar</div>
   )
 }
-
-export default Searchbar
