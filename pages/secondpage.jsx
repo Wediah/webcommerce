c87ee2ec18e56b5c0bc4ffdeaaf7b4cf5e3541af
Wediah <emmanuelwediah@gmail.com>
2022-12-17 01:08:41 +0000

    scroll type changed

--- pages/secondpage.jsx
+++ pages/secondpage.jsx
@@ -32,12 +32,12 @@ function Secondpage() {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
-      slidesToSlide: 5,
+      slidesToSlide: 2,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 3,
-      slidesToSlide: 3,
+      slidesToSlide: 2,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
@@ -59,8 +59,8 @@ function Secondpage() {
           hot tours 
         </h1>
         <Carousel 
-        swipeable={false}
-        draggable={false}
+        swipeable={true}
+        draggable={true}
         showDots={true}
         responsive={Responsive}
         ssr={true} // means to render carousel on server-side.
