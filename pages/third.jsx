0af984466a87ea845e29d7b631b08dbe02222990
Wediah <emmanuelwediah@gmail.com>
2022-12-17 16:15:42 +0000

    3rd page added

---
+++ pages/third.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Thirdpage() {
+  return (
+    <div className='min-h-screen'>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Thirdpage;
