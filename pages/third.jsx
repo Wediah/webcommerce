eb4fe3125448ca4e73058bc89e3cdf616955925f
Wediah <emmanuelwediah@gmail.com>
2022-12-17 16:20:30 +0000

    first div added

--- pages/third.jsx
+++ pages/third.jsx
@@ -4,8 +4,22 @@ function Thirdpage() {
   return (
     <div className='min-h-screen'>
         <div>
-            <div></div>
-            <div></div>
+            <div>
+                <p>About E&M</p>
+                <p>Careers</p>
+                <p>Press</p>
+                <p>Investor Relations</p>
+                <p>Trust & Safety</p>
+                <p>Content Guidelines</p>
+                <p>Accessibility Statement</p>
+                <p>Terms of Service</p>
+                <p>Privacy Policy</p>
+                <p>Ad Choices</p>
+                <p>Your Privacy Choices</p>
+            </div>
+            <div>
+                
+            </div>
             <div></div>
             <div></div>
         </div>
