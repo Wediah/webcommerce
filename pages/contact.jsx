d18e68ab92f5828fa1a35714c21ccf55f8edac5b
Wediah <emmanuelwediah@gmail.com>
2022-12-21 08:15:36 +0000

    padding buttom added to contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -15,7 +15,7 @@ function Contact() {
                       <h3 className="pt-8 pb-2 text-3xl font-bold">Email</h3>
                       <a href=""><h4 className="text-md font-semi-bold">infoE&M@gmail.com</h4></a>
                     </div>
-                    <div className="shadow-2xl rounded-3xl text-center p-10 my-10 flex-1 hover:transition hover:scale-95">
+                    <div className="shadow-2xl rounded-3xl text-center p-10 py-10 pb-20 flex-1 hover:transition hover:scale-95">
                       <h3 className="pt-8 pb-2 text-3xl font-bold">Phone</h3>
                       <h4 className="text-md font-semi-bold ">(+233) 20 000 1111</h4>
                       <h4 className="text-md font-semi-bold ">(+233) 30 111 2222</h4>
