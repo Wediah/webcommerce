195d00b2540fd0e7f9de571ebfea59cc462b8fde
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-03 10:44:16 +0000

    popover removed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -102,21 +102,8 @@ function Navbar () {
                         </div>
 
                         <div className="flex justify-center items-center flex-shrink-0 text-white">
-                            <Popover>
-                            <Popover.Button className="text-sm border-solid border-2 border-orange-400 p-2 px-3 py-1 rounded-full text-black font-bold hover:transition hover:scale-105 duration-100">log in</Popover.Button>
-
-                            <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-200"
-                                enterFrom="opacity-0 translate-y-1"
-                                enterTo="opacity-100 translate-y-0"
-                                leave="transition ease-in duration-150"
-                                leaveFrom="opacity-100 translate-y-0"
-                                leaveTo="opacity-0 translate-y-1"
-                            >
-                            <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl text-black"><Login/></Popover.Panel>
-                            </Transition>
-                            </Popover>
+                            
+                            <button className="text-sm border-solid border-2 border-orange-400 p-2 px-3 py-1 rounded-full text-black font-bold hover:transition hover:scale-105 duration-100">log in</button>
                             <button className="text-sm border-solid border-2 border-orange-400 p-2 px-3 py-1 rounded-full ml-2 text-black bg-orange-400 font-bold hover:transition hover:scale-105 duration-100">register</button>
                         </div>
                         
