ad718b04a457e9b0e978c29bad42367bc4d7ac94
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-05 23:19:04 +0000

    shadow to the forms

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -41,9 +41,9 @@ function Contact() {
     }
   return (
     <Element id="contact" name="contact">
-        <div className='min-h-screen bg-white'>
+        <div className='min-h-screen bg-white px-10'>
             <h1 className='text-center text-xl font-bold pt-40 '>Contact Us</h1>
-            <form className='flex flex-col items-center float-left bg-gray-300 rounded-3xl mx-20 mt-10' onSubmit={handleSubmit}>
+            <form className='flex flex-col items-center float-left  bg-slate-300 rounded-3xl shadow-2xl mr-5 mt-10' onSubmit={handleSubmit}>
               <h1 className='text-sm p-5'>Talk to us and we will get back to you in the shortest time possible</h1>
               <label className='text-gray-500' htmlFor="first">First Name</label>
               <input className='p-2 rounded-2xl' type="text" id="first" name="first" required />
@@ -60,22 +60,24 @@ function Contact() {
               <button className='bg-orange-500 p-2 m-2 rounded-xl hover:transition hover:scale-95' type="submit">Submit</button>
             </form>
 
-            <div className=" gap-5 lg:flex mt-10 px-10 pb-10">
-                  <div className='flex-row bg-yellow-500'>
-                    <div className="shadow-2xl rounded-3xl text-center p-2  flex-1 w-80 bg-slate-300 mt-5">
+            <div className=" gap-5 lg:flex mt-10  pb-10">
+                  <div className='flex-row'>
+                    <div className="shadow-2xl rounded-3xl text-center p-10 flex-1 w-80 bg-slate-300 mt-5">
                       <h3 className="pt-2 pb-2 text-3xl font-bold">Address</h3>
                       <h4 className="text-md font-semi-bold ">Accra, Ghana</h4>
                     </div>
-                    <div className="shadow-2xl rounded-3xl text-center p-2  flex-1 w-80 bg-slate-300 mt-5">
+                    <div className="shadow-2xl rounded-3xl text-center p-10 flex-1 w-80 bg-slate-300 mt-5">
                       <h3 className="pt-2 pb-2 text-3xl font-bold">Email</h3>
                       <a href=""><h4 className="text-md font-semi-bold">infoE&M@gmail.com</h4></a>
                     </div>
                   </div>
                     
-                    <div className="shadow-2xl rounded-3xl text-center p-2  flex-1 w-80 bg-slate-300 mt-5 ">
+                    <div className="shadow-2xl rounded-3xl text-center p-10  flex-1 w-80 bg-slate-300 mt-5 ">
                       <h3 className="pt-2 pb-2 text-3xl font-bold">Phone</h3>
                       <h4 className="text-md font-semi-bold ">(+233) 20 000 1111</h4>
                       <h4 className="text-md font-semi-bold ">(+233) 30 111 2222</h4>
+
+                      
                     </div>
                 </div>
         </div>
