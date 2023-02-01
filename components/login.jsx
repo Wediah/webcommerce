9495d7a112d98b834c869290bb1a754a476f59be
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-01 20:10:59 +0000

    edits sign in button

--- components/login.jsx
+++ components/login.jsx
@@ -8,7 +8,7 @@ import { useRouter } from 'next/router'
 function Login() {
 
   const router = useRouter()
-  
+
   return (
     <div className='min-h-screen relative flex flex-col justify-center overflow-hidden bg-white py-6 sm:py-12'>
       <div className='relative mx-auto w-full max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10'>
@@ -30,12 +30,12 @@ function Login() {
                     <label htmlFor="password" className='pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peerr-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800'>Password</label>
                 </div>
                 <div className="my-6">
-                    <button type="submit" className="w-full rounded-md bg-black px-3 py-4 text-white focus:bg-gray-600 focus:outline-none">Sign in</button>
+                    <button type="submit" className="w-full rounded-md bg-black px-3 py-4 text-white focus:bg-gray-600 focus:outline-none">SLog in</button>
                 </div>
                 <p className="text-center text-sm text-gray-500">Don&#x27;t have an account yet? <a href="#!" className="font-semibold text-gray-600 hover:underline focus:text-gray-800 focus:outline-none" onClick={() => router.push('/Signup')}>Sign up</a>.</p>
                 <p className='text-center text-sm text-gray-500'>or</p>
                 <div className='flex gap-5 justify-center mt-2'>
-                    <button className='text-sm flex bg-black h-8 text-white p-2 font-semibold rounded-md cursor-pointer'><FcGoogle className='text-lg align-middle mr-1 '/>Sign in with Google</button>
+                    <button className='text-sm flex bg-black h-8 text-white p-2 font-semibold rounded-md cursor-pointer'><FcGoogle className='text-lg align-middle mr-1 '/>Log in with Google</button>
                     <button className='text-sm flex bg-black h-8 text-white p-2 font-semibold rounded-md cursor-pointer'><BsApple className='text-lg align-middle mr-1'/>Continue with Apple</button>
                 </div>
             </form>
