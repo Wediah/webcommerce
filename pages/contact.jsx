baaff38ecded8404ac9b0c81db690789d59319b2
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-01 23:08:50 +0000

    edits layout of contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -45,53 +45,53 @@ function Contact() {
     <Element id="contact" name="contact">
         <div className='min-h-screen bg-white px-10'>
             <h1 className='text-center text-xl font-bold pt-40 '>Contact Us</h1>
-            <form className='flex flex-col items-center bg-slate-300 rounded-3xl shadow-2xl mt-10' onSubmit={handleSubmit}>
-              <h1 className='text-sm p-5 text-gray-500'>Talk to us and we will get back to you in the shortest time possible</h1>
-              <label className='text-gray-500' htmlFor="name">First Name</label>
-              <input className='p-2 w-auto rounded-lg' type="text" id="first" name="first" required />
+            <div className='lg:flex gap-5 pb-5'>
+              <div><form className='flex flex-col items-center bg-slate-300 rounded-3xl shadow-2xl mt-10' onSubmit={handleSubmit}>
+                <h1 className='text-sm p-5 text-gray-500'>Talk to us and we will get back to you in the shortest time possible</h1>
+                <label className='text-gray-500' htmlFor="name">First Name</label>
+                <input className='p-2 w-auto rounded-lg bg-gray-100' type="text" id="first" name="first" required />
 
-              <label className='text-gray-500' htmlFor="name">Last Name</label>
-              <input className='p-2  rounded-lg' type="text" id="last" name="last" required />
+                <label className='text-gray-500' htmlFor="name">Last Name</label>
+                <input className='p-2  rounded-lg bg-gray-100' type="text" id="last" name="last" required />
 
-              <label className='text-gray-500' htmlFor="email">Email</label>
-              <input className='p-2  rounded-lg' type="text" id="email" name="email" required />
+                <label className='text-gray-500' htmlFor="email">Email</label>
+                <input className='p-2  rounded-lg bg-gray-100' type="text" id="email" name="email" required />
 
-              <label className='text-gray-500' htmlFor="message">Message</label>
-              <textarea type="text"  className='p-2  rounded-lg h-20 ' required />
+                <label className='text-gray-500' htmlFor="message">Message</label>
+                <textarea type="text"  className='p-2 bg-gray-100 rounded-lg h-20 ' required />
 
-              <button className='bg-orange-500 p-2 m-2 rounded-xl hover:transition hover:scale-95 w-40 text-white' type="submit">Submit</button>
-            </form>
+                <button className='bg-orange-500 p-2 m-2 rounded-xl hover:transition hover:scale-95 w-40 text-white' type="submit">Submit</button>
+              </form></div>
 
-            <div className=" gap-5 lg:flex mt-10  pb-10">
-                  <div className='flex-row'>
-                    <div className="shadow-2xl rounded-3xl text-center p-10 flex-1 w-80  bg-slate-300 mt-5">
-                      <h3 className="pt-2 pb-2 text-3xl font-bold">Address</h3>
-                      <h4 className="text-md font-semi-bold ">Accra, Ghana</h4>
-                    </div>
-                    <div className="shadow-2xl rounded-3xl text-center p-10 flex-1 w-80 bg-slate-300 mt-5">
-                      <h3 className="pt-2 pb-2 text-3xl font-bold">Email</h3>
-                      <a href=""><h4 className="text-md font-semi-bold">infoE&M@gmail.com</h4></a>
+              <div className=" gap-5 lg:flex mt-10 justify-center pb-10">
+                <div className='flex-row'>
+                  <div className="shadow-2xl rounded-3xl text-center p-10 w-80  bg-slate-300 mt-5">
+                    <h3 className="pt-2 pb-2 text-3xl font-bold">Address</h3>
+                    <h4 className="text-md font-semi-bold ">Accra, Ghana</h4>
+                  </div>
+                  <div className="shadow-2xl rounded-3xl text-center p-10 w-80 bg-slate-300 mt-5">
+                    <h3 className="pt-2 pb-2 text-3xl font-bold">Email</h3>
+                    <a href=""><h4 className="text-md font-semi-bold">infoE&M@gmail.com</h4></a>
+                  </div>
+                </div>
+                  
+                  <div className="shadow-2xl rounded-3xl text-center p-10 w-80 bg-slate-300 mt-5 ">
+                    <h3 className="pt-2 pb-2 text-3xl font-bold">Phone</h3>
+                    <h4 className="text-md font-semi-bold ">(+233) 20 000 1111</h4>
+                    <h4 className="text-md font-semi-bold ">(+233) 30 111 2222</h4>
+                    <div className='flex p-10 items-center '>
+                      <AiFillFacebook className='h-10 w-20 hover:translate-y-2.5'/>
+                      <FaFacebookMessenger className='h-10 w-20 hover:translate-y-2.5'/>
+                      <AiFillInstagram className='h-10 w-20 hover:translate-y-2.5'/>
+                      <AiFillTwitterSquare className='h-10 w-20 hover:translate-y-2.5'/>
+                      <AiFillLinkedin className='h-10 w-20 hover:translate-y-2.5'/>
+                      <AiOutlineWhatsApp className='h-10 w-20 hover:translate-y-2.5'/>
+                      <AiFillWechat className='h-10 w-20 hover:translate-y-2.5'/>
+                      <FaTiktok className='h-10 w-20 hover:translate-y-2.5'/>
                     </div>
                   </div>
-                    
-                    <div className="shadow-2xl rounded-3xl text-center p-10  flex-1 w-80 bg-slate-300 mt-5 ">
-                      <h3 className="pt-2 pb-2 text-3xl font-bold">Phone</h3>
-                      <h4 className="text-md font-semi-bold ">(+233) 20 000 1111</h4>
-                      <h4 className="text-md font-semi-bold ">(+233) 30 111 2222</h4>
-                      <div className='flex p-10 items-center '>
-                        <AiFillFacebook className='h-10 w-20 hover:translate-y-2.5'/>
-                        <FaFacebookMessenger className='h-10 w-20 hover:translate-y-2.5'/>
-                        <AiFillInstagram className='h-10 w-20 hover:translate-y-2.5'/>
-                        <AiFillTwitterSquare className='h-10 w-20 hover:translate-y-2.5'/>
-                        <AiFillLinkedin className='h-10 w-20 hover:translate-y-2.5'/>
-                        <AiOutlineWhatsApp className='h-10 w-20 hover:translate-y-2.5'/>
-                        <AiFillWechat className='h-10 w-20 hover:translate-y-2.5'/>
-                        <FaTiktok className='h-10 w-20 hover:translate-y-2.5'/>
-                      </div>
-                      
-
-                    </div>
-                </div>
+              </div>
+          </div>
         </div>
     </Element>
   )
