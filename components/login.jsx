5422d8b89af882aba45fff3ecdb51cff9b353980
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-01 19:51:06 +0000

    puts from in a div

--- components/login.jsx
+++ components/login.jsx
@@ -15,7 +15,8 @@ function Login() {
         <div className='basis-1/3 flex-1  '>
           <div className=' font-bold text-2xl text-orange-500 pl-10 pt-10 '>Login</div> 
           <div className=' text-sm text-gray-400 pl-10'>Welcome back! Please fill in your details.</div>
-          <form action="">
+          <div className='mt-5'>
+                        <form action="">
                             <div className='relative mt-6'>
                                 <input type="email" name='email' id='email' placeholder='Email Address' className='peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500m focus:outline-none' autoComplete='NA' />
                                 <label htmlFor="email" className='pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peerr-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800'>Email Address</label>
@@ -34,6 +35,7 @@ function Login() {
                                 <button className='text-sm flex bg-black h-8 text-white p-2 font-semibold rounded-md cursor-pointer'><BsApple className='text-lg align-middle mr-1'/>Continue with Apple</button>
                             </div>
                         </form>
+                    </div>
         </div>
       </div>
       
