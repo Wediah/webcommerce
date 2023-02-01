0a2623036e3c6477b8db2782ba9084fea245904c
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-01 19:48:16 +0000

    adds sign in form

--- components/login.jsx
+++ components/login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/legacy/image';
 import bali from '../public/bali.jpg'
+import {BsApple} from 'react-icons/bs'
+import {FcGoogle} from 'react-icons/fc'
 
 function Login() {
   return (
@@ -13,20 +15,25 @@ function Login() {
         <div className='basis-1/3 flex-1  '>
           <div className=' font-bold text-2xl text-orange-500 pl-10 pt-10 '>Login</div> 
           <div className=' text-sm text-gray-400 pl-10'>Welcome back! Please fill in your details.</div>
-          <form action="/send-data-here" method="post" className='flex flex-col gap-2 pl-10 pt-5 '>
-              <label for="email / phone" className='font-bold text-sm'>email or phone</label>
-              <input
-                  type="text"
-                  id="email"
-                  name="email"
-                  required
-                  minlength="10"
-                  className='bg-gray-300 rounded-2xl h-8 w-46'
-              />
-              <label for="password" className='font-bold text-sm'>password</label>
-              <input type="password" id="password" name="password" required className='bg-gray-300 rounded-2xl h-8 w-46'/>
-              <button type="submit" className=' bg-orange-500 h-8 w-20 rounded-full font-bold text-white'>log in</button>
-          </form>
+          <form action="">
+                            <div className='relative mt-6'>
+                                <input type="email" name='email' id='email' placeholder='Email Address' className='peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500m focus:outline-none' autoComplete='NA' />
+                                <label htmlFor="email" className='pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peerr-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800'>Email Address</label>
+                            </div>
+                            <div className='relative mt-6'>
+                                <input type="password" name='password' id='password' placeholder='Password' className='peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500m focus:outline-none' />
+                                <label htmlFor="password" className='pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peerr-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800'>Password</label>
+                            </div>
+                            <div className="my-6">
+                                <button type="submit" className="w-full rounded-md bg-black px-3 py-4 text-white focus:bg-gray-600 focus:outline-none">Sign in</button>
+                            </div>
+                            <p className="text-center text-sm text-gray-500">Don&#x27;t have an account yet? <a href="#!" className="font-semibold text-gray-600 hover:underline focus:text-gray-800 focus:outline-none" onClick={() => router.push('/Signup')}>Sign up</a>.</p>
+                            <p className='text-center text-sm text-gray-500'>or</p>
+                            <div className='flex gap-5 justify-center mt-2'>
+                                <button className='text-sm flex bg-black h-8 text-white p-2 font-semibold rounded-md cursor-pointer'><FcGoogle className='text-lg align-middle mr-1 '/>Sign in with Google</button>
+                                <button className='text-sm flex bg-black h-8 text-white p-2 font-semibold rounded-md cursor-pointer'><BsApple className='text-lg align-middle mr-1'/>Continue with Apple</button>
+                            </div>
+                        </form>
         </div>
       </div>
       
