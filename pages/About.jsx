b5c1b84ebd5e12f9e08de74206b17e735e084ce5
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-03 14:39:16 +0000

    centers spans

--- pages/About.jsx
+++ pages/About.jsx
@@ -11,27 +11,27 @@ function About() {
         <div className='min-h-screen bg-white'>
             <h1 className='text-black px-8 text-3xl font-bold pt-40'>About Us</h1>
             <div className='flex flex-wrap gap-20 pt-10 flex-row justify-center px-5'>
-                <div className='flex'>
+                <div className='flex items-center'>
                     <span className='bg-blue-500 rounded-full p-5 font-bold text-xl  '>15</span>
                     <p className='ml-2 text-lg font-semibold'> years old <br /> <span className='text-sm font-thin'>in the tourism business</span></p>
                 </div>
-                <div className='flex '>
+                <div className='flex items-center'>
                     <span className='bg-blue-500 rounded-full p-5 font-bold text-xl'>1M+</span>
                     <p className='ml-2 text-lg font-semibold'>tourist <br /> <span className='text-sm font-thin'>travel with us <br /> every month</span></p>
                 </div>
-                <div className='flex '>
+                <div className='flex items-center'>
                 <span className='bg-blue-500 rounded-full p-5 font-bold text-xl'>506</span>
                     <p className='ml-2 text-lg font-semibold'>offices <br /> <span className='text-sm font-thin' > in 100 countries</span> </p>
                 </div>
-                <div className='flex '>
+                <div className='flex items-center'>
                     <span className='bg-blue-500 rounded-full p-5 font-bold text-xl'>132</span>
                     <p className='ml-2 text-lg font-semibold'>countries <br /> <span className='text-sm font-thin' >you can visit</span> </p>
                 </div>
-                <div className='flex '>
+                <div className='flex items-center'>
                     <span className='bg-blue-500 rounded-full p-5 font-bold text-xl'>98%</span>
                     <p className='ml-2 text-lg font-semibold'>satisfied <br /> <span className='text-sm font-thin' >tourists world wide</span></p>
                 </div>
-                <div className='flex  '>
+                <div className='flex  items-center'>
                     <span className='bg-blue-500 rounded-full p-5 font-bold text-xl'>62</span>
                     <p className='ml-2 text-lg font-semibold'>thousand <br /> <span className='text-sm font-thin'>hotels world wide</span> </p>
                 </div>
