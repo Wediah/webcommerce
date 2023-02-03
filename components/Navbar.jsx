982de40c79d4b0283cf63fd7467c841cad5ec0a5
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-03 14:48:15 +0000

    removes border radius

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar () {
 
 
 	return (
-        <nav className="fixed w-full z-20 bg-white shadow-2xl rounded-lg">
+        <nav className="fixed w-full z-20 bg-white shadow-2xl">
             <div className="w-full">
                 <div className="flex items-center h-20 w-full">
                     <div className="flex items-center  mx-20  justify-between w-full">
