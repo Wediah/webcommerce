4491660cf7eb2944af653bb5d3f4091db8fd4f5d
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-08 00:01:28 +0000

    adds text size

--- pages/third.jsx
+++ pages/third.jsx
@@ -78,7 +78,7 @@ function Thirdpage() {
             </div>
         </div>
 
-        <h1 className="pt-10 text-center">Copyright ©2020-2026 E&M <h1>built by Emmanuel Wediah</h1></h1> 
+        <h1 className="pt-10 text-center">Copyright ©2020-2026 E&M <h1 className='text-sm'>built by Emmanuel Wediah</h1></h1> 
         
     </div>
   )
